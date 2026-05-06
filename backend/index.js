cccfd4b63feddaf9f6e8623ebfbf744c10575546
luaygitaris <luaygitaris.com>
2026-05-06 10:41:46 +0700

    menambahkan cors agar bisa beberepa

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,23 @@ const { nanoid } = require('nanoid');
 dotenv.config();
 
 const app = express();
-app.use(cors());
+const allowedOrigins = process.env.ALLOWED_ORIGINS 
+  ? process.env.ALLOWED_ORIGINS.split(',') 
+  : ['http://localhost:5173'];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    // Allow requests with no origin (like mobile apps or curl requests)
+    if (!origin) return callback(null, true);
+    
+    if (allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true
+}));
 app.use(express.json());
 
 // Firebase Admin Initialization
